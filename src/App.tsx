71f11628fee626564d5d035a3a5be9e2603896d9
Dominik <domi.kowalik@gmail.com>
2021-08-04 11:34:09 +0200

    Add ShopPage, CollectionPreview component and CollectionItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router";
 
 import "./App.scss";
 import { HomePage } from "./Pages/HomePage";
+import { ShopPage } from "./Pages/ShopPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
