d35c5488b54fe9421b0e5b6892baeae6877a91e5
Dominik <domi.kowalik@gmail.com>
2021-08-05 17:16:12 +0200

    Add store and implement to provider in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import store from "./redux/store";
+
 import "./index.scss";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
