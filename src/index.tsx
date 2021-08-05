5e9d5a8cf720b35c96709865701eceda461eba26
Dominik <domi.kowalik@gmail.com>
2021-08-05 17:04:17 +0200

    install redux-logger

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    {/* <Provider> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
