e281fe157fa991931e545ecd4dafd2934bb15d64
Dominik <domi.kowalik@gmail.com>
2021-08-18 15:26:48 +0200

    Implement redux-persist to localstorage card and user slice of store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import loggerMiddleware from "redux-logger";
-import thunkMiddleware from "redux-thunk";
+import thunkMiddleware, { ThunkMiddleware } from "redux-thunk";
 
 import cardReducer from "./card/card.slice";
 import userReducer from "./user/user.slice";
 import directoryReducer from "./directory/directory.slice";
 import collectionReducer from "./collection/collection.slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const reducers = {
+const rootReducer = combineReducers({
   user: userReducer,
   card: cardReducer,
   directory: directoryReducer,
   collection: collectionReducer,
+});
+
+let middlewares = [thunkMiddleware as ThunkMiddleware];
+
+if (process.env.NODE_ENV === "development")
+  middlewares = [...middlewares, loggerMiddleware];
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["user", "card"],
 };
 
-const middlewares = [thunkMiddleware, loggerMiddleware];
-
-const store = configureStore({
-  reducer: reducers,
+export const store = configureStore({
+  reducer: persistReducer(persistConfig, rootReducer),
   middleware: middlewares,
 });
 
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
